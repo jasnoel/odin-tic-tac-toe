b79537d6a72bee7d2eb3ce5d999b426539a6c68b
jasnoel <jason.noel.imp@gmail.com>
2022-04-23 12:09:30 +0200

    created UI module with display function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameBoard = (() => {
-    let board = [['', '', ''], ['', '', ''], ['', '', '']];
+    let board = [['', 'X', ''], ['', 'O', ''], ['X', '', '']];
 
     const getBoard = () => board;
 
@@ -11,9 +11,45 @@ const gameBoard = (() => {
         }
     }
 
+    function display() {
+    }
+
     function reset() {
         board = [['', '', ''], ['', '', ''], ['', '', '']];
     }
 
-    return {getBoard, placeTile, reset}
+    return {getBoard, placeTile, reset, display}
 })();
+
+const UI = (() => {
+    let board = [];
+
+    const mainDiv = document.querySelector('.game-board');
+    for (let x = 0; x < 3; x++) {
+        let tileX = document.createElement('div')
+        tileX.classList.add('x');
+        mainDiv.appendChild(tileX);
+
+        let boardX = [];
+        for (let y = 0; y < 3; y++) {
+            let tileY = document.createElement('div');
+            tileY.classList.add('y');
+            tileX.appendChild(tileY);
+            boardX.push(tileY);
+        }
+        board.push(boardX);
+    }
+
+    function display(grid) {
+        console.log(grid);
+        board.forEach((lign, x) => {
+            lign.forEach((element, y) => {
+                element.textContent = grid[x][y];
+            })
+        });
+    }
+
+    return { display };
+})();
+
+UI.display(gameBoard.getBoard());
