094d1e722fb40d083d0c5224624a3553d81737ec
jasnoel <jason.noel.imp@gmail.com>
2022-04-24 16:20:58 +0200

    Players factory function + usernames

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const Player = (name, sign) => {
+    
+    const getName = () => name;
+    const getSign = () => sign;
+
+    return { getName, getSign }
+}
+
 const gameBoard = (() => {
     let board = [['', '', ''], ['', '', ''], ['', '', '']];
 
@@ -20,10 +28,20 @@ const gameBoard = (() => {
 })();
 
 const game = (() => {
-    let sign = 'X';
+    let player1 = Player('player1', 'X');
+    let player2 = Player('player2', 'O');
+    let currentPlayer = player1;
 
     function switchSign() {
-        sign = sign == 'X' ? 'O' : 'X';
+        currentPlayer = currentPlayer.getSign() == 'X' ? player2 : player1;
+    }
+
+    function start() {
+        let playerName = document.querySelector('.player1').lastElementChild.value || 'Player 1';
+        player1 = Player(playerName, 'X');
+        playerName = document.querySelector('.player2').lastElementChild.value || 'Player 2';
+        player2 = Player(playerName, 'O');
+        UI.showMessage(`${player1.getName().toUpperCase()} vs ${player2.getName().toUpperCase()}`)
     }
     
     function restart() {
@@ -35,11 +53,11 @@ const game = (() => {
     function end() {
         UI.switchAccess(false)
         UI.displayRestart(true);
-        UI.showMessage(`${sign} won the game!`);
+        UI.showMessage(`${currentPlayer.getName()} won the game!`);
     }
 
     function placeTile(x, y) {
-        if (gameBoard.placeTile(x, y, sign) == true) {
+        if (gameBoard.placeTile(x, y, currentPlayer.getSign()) == true) {
             UI.display();
             if (checkWin() == true) {
                 UI.displayRestart(true);
@@ -69,7 +87,7 @@ const game = (() => {
         return false;
     }
 
-    return { placeTile, restart }
+    return { placeTile, restart, start }
 })();
 
 const UI = (() => {
@@ -79,30 +97,22 @@ const UI = (() => {
     const mainDiv = document.querySelector('.game-board');
     const cache = mainDiv.firstElementChild;
     const restart = document.querySelector('.restart');
+    const startBtn = document.querySelector('.start-btn');
 
+    //CONFIURE START // RESTART GAME BTN
+    startBtn.addEventListener('click', () => {
+        UI.instanciateBoard();
+        startBtn.classList.add('inactive')
+        const playerForm1 = document.querySelector('.player1');
+        playerForm1.classList.add('inactive');
+        const playerForm2 = document.querySelector('.player2');
+        playerForm2.classList.add('inactive');
+        game.start();
+    });
     restart.addEventListener('click', () => {
         displayRestart(false);
         game.restart();
     });
-
-    for (let x = 0; x < 3; x++) {
-        let tileX = document.createElement('div')
-        tileX.classList.add('x');
-        mainDiv.appendChild(tileX);
-
-        let boardX = [];
-        for (let y = 0; y < 3; y++) {
-            let tileY = document.createElement('div');
-            tileY.classList.add('y');
-            tileX.appendChild(tileY);
-            boardX.push(tileY);
-
-            tileY.addEventListener('click', () => {
-                game.placeTile(x, y);
-            });
-        }
-        board.push(boardX);
-    }
 
     function showMessage(msg) {
         messageDiv.textContent = msg;
@@ -124,7 +134,29 @@ const UI = (() => {
         }
     }
 
+    function instanciateBoard() {
+        for (let x = 0; x < 3; x++) {
+            let tileX = document.createElement('div')
+            tileX.classList.add('x');
+            mainDiv.appendChild(tileX);
+    
+            let boardX = [];
+            for (let y = 0; y < 3; y++) {
+                let tileY = document.createElement('div');
+                tileY.classList.add('y');
+                tileX.appendChild(tileY);
+                boardX.push(tileY);
+                
+                tileY.addEventListener('click', () => {
+                    game.placeTile(x, y);
+                });
+            }
+            board.push(boardX);
+        }
+    }
+
     function display() {
+        
         let grid = gameBoard.getBoard();
         board.forEach((lign, x) => {
             lign.forEach((element, y) => {
@@ -133,7 +165,5 @@ const UI = (() => {
         });
     }
 
-    return { display, switchAccess, showMessage, displayRestart };
+    return { display, switchAccess, showMessage, displayRestart, instanciateBoard };
 })();
-
-UI.display();
